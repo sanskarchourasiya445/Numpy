c763f3ee247176c685c77bcd408721536a682d4b
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-01-15 07:50:56 +0530

    Add files via upload

---
+++ MultidimenasionalArray.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# x = np.array([1,2,3,4,5])
+# print(x)
+# print(type(x))
+# print(x.ndim)
+
+# l = []
+# for i in range(1,6):
+#     l.append(int(input("Enter a number :")))
+
+# print(np.array(l))    
+
+
+# arr = np.array([['A','B','C'],['D','E','F'],['G','H','I']])
+# print(arr)
+# print(arr.ndim)
+
+# y = np.array([[[1,2],[3,4]], [[5,6],[7,8]]])
+# print(y)
+# print(y.ndim)
+
+# arn = np.array([1,2,3,4], ndmin = 10)
+# print(arn)
+# print(arn.ndim)
