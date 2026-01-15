0f2fd73e1551910f5af8c65592d77343fc43c1f7
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-01-15 07:54:31 +0530

    Created MultidimenasionalArray.py

--- MultidimenasionalArray.py
+++ MultidimenasionalArray.py
@@ -1,25 +1,25 @@
 import numpy as np
+# A NumPy array is a fast, efficient data structure
+# used for numerical and scientific computing.
+x = np.array([1,2,3,4,5])
+print(x)
+print(type(x))
+print(x.ndim)
 
-# x = np.array([1,2,3,4,5])
-# print(x)
-# print(type(x))
-# print(x.ndim)
-
-# l = []
-# for i in range(1,6):
-#     l.append(int(input("Enter a number :")))
-
-# print(np.array(l))    
+arr = []
+for i in range(1,6):
+    arr.append(int(input("Enter a number :")))
+print(np.array(arr))    
 
 
-# arr = np.array([['A','B','C'],['D','E','F'],['G','H','I']])
-# print(arr)
-# print(arr.ndim)
+arr2 = np.array([['A','B','C'],['D','E','F'],['G','H','I']])
+print(arr2)
+print(arr2.ndim)
 
-# y = np.array([[[1,2],[3,4]], [[5,6],[7,8]]])
-# print(y)
-# print(y.ndim)
+ar3 = np.array([[[1,2],[3,4]], [[5,6],[7,8]]])
+print(arr3)
+print(arr3.ndim)
 
-# arn = np.array([1,2,3,4], ndmin = 10)
-# print(arn)
-# print(arn.ndim)
+arrN = np.array([1,2,3,4], ndmin = 10)
+print(arrN)
+print(arrN.ndim)
